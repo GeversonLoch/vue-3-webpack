d373c28fa1357b3b7489ed5c08e749c76f29dc35
Geverson Loch <geverson.loch@uniodigital.com.br>
2022-09-13 16:38:02 -0300

    Disable unused route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::users('/users', function() {
-    User::all();
-});
+// Route::users('/users', function() {
+//     User::all();
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
